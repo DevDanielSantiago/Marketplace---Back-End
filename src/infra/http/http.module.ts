ed7f18d429688587748aea3d4e4d35b89e77c705
Daniel Santiago <dev.danielsantiago@gmail.com>
2025-09-12 19:55:25 -0400

    feat: categories

--- src/infra/http/http.module.ts
+++ src/infra/http/http.module.ts
@@ -2,7 +2,11 @@ import { Module } from '@nestjs/common'
 
 import { AuthenticateSellerController } from './controllers/authenticate-seller.controller'
 import { RegisterSellerController } from './controllers/register-seller.controller'
+import { CreateCategoryController } from './controllers/create-category.controller'
+import { FetchAllCategoriesController } from './controllers/fetch-all-categories.controller'
+import { CreateCategoryUseCase } from '@/domain/marketplace/application/use-cases/create-category'
 import { DatabaseModule } from '../database/database.module'
+import { FetchAllCategoriesUseCase } from '@/domain/marketplace/application/use-cases/fetch-all-categories'
 import { CryptographyModule } from '../cryptography/cryptography.module'
 import { RegisterSellerUseCase } from '@/domain/marketplace/application/use-cases/register-seller'
 import { AuthenticateSellerUseCase } from '@/domain/marketplace/application/use-cases/authenticate-seller'
@@ -25,6 +29,8 @@ import { SignOutController } from './controllers/sign-out.controller'
     EditSellerController,
     GetSellerProfileController,
     AuthenticateSellerController,
+    CreateCategoryController,
+    FetchAllCategoriesController,
     UploadAttachmenstController,
     GetAttachmentContentController,
     SignOutController,
@@ -33,6 +39,8 @@ import { SignOutController } from './controllers/sign-out.controller'
     RegisterSellerUseCase,
     EditSellerUseCase,
     GetSellerProfileUseCase,
+    CreateCategoryUseCase,
+    FetchAllCategoriesUseCase,
     AuthenticateSellerUseCase,
     RegisterProductViewUseCase,
     UploadAndCreateAttachmentUseCase,
